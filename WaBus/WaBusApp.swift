2c79c44806e4a154cebe67a94418e332a2187b3e
Oleksandr Drobinin <amperescom@gmail.com>
2026-02-05 22:31:18 +0100

    Initial Commit

---
+++ WaBus/WaBusApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaBusApp.swift
+//  WaBus
+//
+//  Created by Oleksandr Drobinin on 05/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct WaBusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
